d5b160d32094ef07a5050fd5960ed4ef19c6e0e9
Shady <shady-abdelhamid@outlook.com>
2018-01-06 08:47:19 +0200

    avoide state mutation - use tassign plugin.

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,4 +1,5 @@
 import { INCREMENT } from './actions';
+import { tassign } from 'tassign'
 
 export interface IAppState{
     counter: number;
@@ -11,7 +12,8 @@ export const INITIAL_STATE: IAppState ={
 export function rootReducer(state: IAppState, action): IAppState{
     switch (action.type) {
         case INCREMENT:
-            return { counter: state.counter + 1 };
+            // tassign is much safer than Object.assign
+            return tassign(state, { counter: state.counter+1})
     }
     return state;
 }
